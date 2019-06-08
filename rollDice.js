e4c50be82f477849971d838050bec391b6319e37
AD Faris <35715329+adfaris@users.noreply.github.com>
2019-06-08 08:08:30 -0400

    Create rollDice.js

---
+++ rollDice.js
@@ -0,0 +1,5 @@
+// 11.
+// Imagine you throw two six sided dice and sum up the total.
+// Write a loop that logs every possible score you could roll.
+// Used a nested for loop ( a loop inside of a loop)
+// Bonus Challenge: Don't log repeats.
