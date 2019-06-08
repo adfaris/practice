cf44297ccddb3072b8c9860cf1f9dcd1f9e2d453
A.D.Faris <adfaris@gmail.com>
2019-06-08 11:17:06 -0400

    rolling 2 dice

--- rollDice.js
+++ rollDice.js
@@ -3,3 +3,8 @@
 // Write a loop that logs every possible score you could roll.
 // Used a nested for loop ( a loop inside of a loop)
 // Bonus Challenge: Don't log repeats.
+let roll1 = [1, 2, 3, 4, 5, 6];
+let roll2 = [1, 2, 3, 4, 5, 6];
+for (let i = 0; i < roll1.length; i++) {
+  for (let j = 0; j < roll2.length; j++) {}
+}
