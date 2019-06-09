d30de705590fa5e528c60b953b047f8fa8843a75
A.D.Faris <adfaris@gmail.com>
2019-06-08 22:46:16 -0400

    forEach

--- rollDice.js
+++ rollDice.js
@@ -3,8 +3,14 @@
 // Write a loop that logs every possible score you could roll.
 // Used a nested for loop ( a loop inside of a loop)
 // Bonus Challenge: Don't log repeats.
-let roll1 = [1, 2, 3, 4, 5, 6];
-let roll2 = [1, 2, 3, 4, 5, 6];
+let roll1 = [1, 2, 3, 4, 5, 6]
+let roll2 = [1, 2, 3, 4, 5, 6]
+let sum = 0
+let result = []
 for (let i = 0; i < roll1.length; i++) {
-  for (let j = 0; j < roll2.length; j++) {}
+  for (let j = 0; j < roll2.length; j++) {
+    console.log(roll1[i], roll2[j], i + j)
+    result.push((sum += roll1[i] + roll2[i]))
+  }
 }
+console.log(result, result.length)
