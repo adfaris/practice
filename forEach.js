5befd40ec68743627e04e5f6ebf34e994ddc09c2
A.D.Faris <adfaris@gmail.com>
2019-07-04 23:09:39 -0400

    forEach

--- forEach.js
+++ forEach.js
@@ -1,12 +1,18 @@
-Array.prototype.forEach = (cb) => {
-    for(let i = 0; i < arr.length; i++) {
-        console.log (arr[i])
-    }
-}
+// Array.prototype.forEach = cb(elements)
 
-function cb () {
-}
-let element = [1, 2, 3, 4, 5, 6, 7, 8]
-element.forEach(cb)
+// function cb(elements) {
+//   for(let index = 0; index < elements.length; i++) {
+//       return elements[index], index
+// }
+// let elements = [1, 2, 3, 4, 5, 6, 7, 8]
+// element.forEach(cb)
 
 // arr.forEach(callback(currentValue[, index[, array]])[, thisArg]);
+
+
+Array.prototype.forEach = cb (element) 
+    // make the code work again
+    // map, filter, reduce, forEac
+
+
+['a', 'b', 'c'].forEach(console.log)
