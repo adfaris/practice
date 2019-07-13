3c90ab195e008329e96b972166034ec9ccc79f32
A.D.Faris <adfaris@gmail.com>
2019-07-13 01:17:38 -0400

    filter

--- filter.js
+++ filter.js
@@ -2,14 +2,14 @@
  * var newArray = arr.filter(callback(element[, index[, array]])[, thisArg])
  */
 
-// const names = ['Josiah', 'Zoe', 'Dina', 'A.D.']
-let nums = [3, 5, 3, 5, 6, 8, 90, 78]
+const names = ['sam', 'Maz', 'Dina']
+// let nums = [3, 5, 3, 5, 6, 8, 90, 78]
 // let finding = ''
 
 const callback = (element, index, array) => {
-  console.log(element, index, array)
-  return element > 3
+//   console.log(element, index, array)
+  return element.length > 3
 }
 
-nums.filter(callback)
+names.filter(callback)
 // console.log(finding)
