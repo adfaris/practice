843f778fd6c9746a7b79549aa41a1fd383f045c6
A.D.Faris <adfaris@gmail.com>
2019-07-12 23:27:53 -0400

    forEach

--- forEach.js
+++ forEach.js
@@ -1,36 +1,4 @@
-// // Array.prototype.forEach = cb(elements)
+Array.prototype.forEach = console.log
 
-// // function cb(elements) {
-// //   for(let index = 0; index < elements.length; i++) {
-// //       return elements[index], index
-// // }
-// const elements = [1, 2, 3, 4, 5, 6, 7, 8]
-// // element.forEach(cb)
+Array.forEach(`Hi my name is A.D. Faris`)
 
-// // arr.forEach(callback(currentValue[, index[, array]])[, thisArg]);
-
-
-// Array.prototype.forEach = function callback(element, index, array ) {
-//     console.log(element, index)
-// } 
-// //     // make the code work again
-// //     // map, filter, reduce, forEac
-
-
-['a', 'b', 'c'].forEach(console.log)
-
-// elements.forEach(element => {
-//     console.log(element)
-// })
-
-function callback(element, index, array) {
-    console.log('a[' + index + '] = ' + element);
-}
-
-// Notice that index 2 is skipped since there is no item at
-// that position in the array.
-[2, 5, , 9].forEach(callback);
-// logs:
-// a[0] = 2
-// a[1] = 5
-// a[3] = 9
