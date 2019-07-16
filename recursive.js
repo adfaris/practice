af6d0c14f51d012a32f61d2af6abffa325d33b43
A.D.Faris <adfaris@gmail.com>
2019-07-15 21:01:56 -0400

    recurse

--- recursive.js
+++ recursive.js
@@ -1,9 +1,9 @@
 class LinkedList {
-  constructor() {
+  constructor () {
     this.head = null
     this.tail = null
   }
-  addNode(data) {
+  addNode (data) {
     const node = {
       data,
       next: null
@@ -16,51 +16,51 @@ class LinkedList {
     if (this.head) {
       this.tail.next = node
       this.tail = node
-      }
-    }
-  
-    // search(searchValue) { // 25 - true // 26 - false
-    //   if (this.head === null) return false;
-  
-    //   let pointer = this.head;
-    //   while (pointer !== null) {
-    //     if (pointer.data === searchValue) return true;
-    //     pointer = pointer.next;
-    //   }
-  
-    //   return false;
-    // }
-  
-    search(searchValue) {
-      // base case - body to execute - move towards the base case
-      let containsValue = false;
-  
-      function searchList(node) {
-        if (node === null) return;
-  
-        if (node.data === searchValue) {
-          containsValue = true;
-          return;
-        }
-  
-        searchList(node.next);
-      }
-      searchList(this.head);
-  
-      return containsValue;
     }
   }
-  
-  const list = new LinkedList();
-  console.log(list.search(15));
-  list.addNode(5);
-  list.addNode(20);
-  list.addNode(10);
-  list.addNode(20);
-  list.addNode(15);
-  list.addNode(20);
-  list.addNode(25);
-  list.addNode(30);
-  list.addNode(20);
-  
-  console.log(list.search(26));
+
+  // search(searchValue) { // 25 - true // 26 - false
+  //   if (this.head === null) return false;
+
+  //   let pointer = this.head;
+  //   while (pointer !== null) {
+  //     if (pointer.data === searchValue) return true;
+  //     pointer = pointer.next;
+  //   }
+
+  //   return false;
+  // }
+
+  search (searchValue) {
+    // base case - body to execute - move towards the base case
+    let containsValue = false
+
+    function searchList (node) {
+      if (node === null) return
+
+      if (node.data === searchValue) {
+        containsValue = true
+        return
+      }
+
+      searchList(node.next)
+    }
+    searchList(this.head)
+
+    return containsValue
+  }
+}
+
+const list = new LinkedList()
+console.log(list.search(15))
+list.addNode(5)
+list.addNode(20)
+list.addNode(10)
+list.addNode(20)
+list.addNode(15)
+list.addNode(20)
+list.addNode(25)
+list.addNode(30)
+list.addNode(20)
+
+console.log(list.search(26))
