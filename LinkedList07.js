e8f67235fe85d6179f64309286895f6b4677943d
A.D.Faris <adfaris@gmail.com>
2019-07-27 23:18:41 -0400

    LL

---
+++ LinkedList07.js
@@ -0,0 +1,28 @@
+class LinkedList {
+  constructor () {
+    this.head = null
+  }
+
+    addToTail (data) {
+      const node = {
+      data: data,
+      next: null
+    }
+    if (this.head === null) {
+      this.head = node
+    } else {
+        while(this.head.next !== null) {
+            this.head = this.head.next
+        }
+        this.head.next = node
+    }
+  }
+}
+
+const list = new LinkedList()
+list.addToTail(5)
+list.addToTail(15)
+list.addToTail(20)
+list.addToTail(25)
+
+console.log(list)
